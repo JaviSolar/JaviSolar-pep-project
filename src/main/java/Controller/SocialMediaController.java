61ef6f09c222c51fc09faea89cb424aa211d7ae4
Javier Solares <javiersolares0414@gmail.com>
2025-01-08 22:23:59 +0000

    Partially completed postAccountHandler method

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -9,6 +13,9 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    // AccountService accountService;
+    // MessageService messageService;
+
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -16,7 +23,9 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        // app.get("example-endpoint", this::exampleHandler);
+        app.post("/account", this::postAccountHandler);
+
 
         return app;
     }
@@ -29,5 +38,16 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
+    private void postAccountHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = accountService.addAuthor(account);
+        if(addedAccount!=null && addedAccount.getPassword().length() >= 4 && addedAccount.getUsername() != null){
+            ctx.json(mapper.writeValueAsString(addedAccount));
+            ctx.status(200);
+        }else{
+            ctx.status(400);
+        }
+    }
 
 }
